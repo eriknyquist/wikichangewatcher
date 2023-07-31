95cee5e3af07cede8f40b6d24400e59f8d7e55b6
Erik Nyquist <eknyquist@gmail.com>
2023-07-30 20:09:24 -0700

    Bump version

--- wikichangewatcher/__init__.py
+++ wikichangewatcher/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.3"
+__version__ = "0.1.0"
 
 from wikichangewatcher.wikichangewatcher import FieldFilter, FilterCollection, IpV4Filter, WikiChangeWatcher, MatchType
 from wikichangewatcher.wikichangewatcher import FieldStringFilter, FieldRegexMatchFilter, FieldRegexSearchFilter
