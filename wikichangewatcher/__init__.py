2df4cbab8fdc7c9ceee5226bfb61f01d58da68aa
Erik Nyquist <eknyquist@gmail.com>
2023-08-06 19:48:48 -0700

    Bump version, update docs build

--- wikichangewatcher/__init__.py
+++ wikichangewatcher/__init__.py
@@ -1,7 +1,7 @@
 __author__ = "Erik Nyquist"
 __license__ = "Apache 2.0"
 __maintainer__ = "Erik Nyquist"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __email__ = "eknyquist@gmail.com"
 
 from wikichangewatcher.wikichangewatcher import FieldFilter, FilterCollection, IpV4Filter, IpV6Filter, WikiChangeWatcher, MatchType
